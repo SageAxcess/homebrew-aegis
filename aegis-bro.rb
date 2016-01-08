751bf7cd77fd38b0649c63aaab2d4d56474829e8
Mikhail Burilov <burilovmv@gmail.com>
2016-01-08 09:05:38 +0600

    added sha sum

--- aegis-bro.rb
+++ aegis-bro.rb
@@ -4,11 +4,9 @@ class AegisBro < Formula
   desc "Network security monitor"
   homepage "https://www.bro.org"
   url "http://itserviceplus.net/aegis-bro.tar.gz"
-  sha256 "d8b99673a5024630f6bae820c4f8c3ca9029f1167f9e5729c914c66e1fc7c8f6"
+  sha256 "0f38ca077dcdf51dc50fc356cfa9d0c0017f699801612543d0c4402525827ca1"
   version "2.4.1"
 
-  depends_on "cmake" => :build
-  depends_on "swig" => :build
   depends_on "openssl"
   depends_on "geoip" => :recommended
 
@@ -20,6 +18,7 @@ class AegisBro < Formula
      include.install Dir["include/*"]
      lib.install Dir["lib/*"]
      share.install Dir["share/*"]
+     etc.install Dir["etc/*"]
   end
 
   test do
