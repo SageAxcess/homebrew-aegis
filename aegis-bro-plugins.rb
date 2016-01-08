4bea26bd7e3bcd4a7c58528876a99f34acdd7ce1
Mikhail Burilov <burilovmv@gmail.com>
2016-01-08 09:15:25 +0600

    fix

--- aegis-bro-plugins.rb
+++ aegis-bro-plugins.rb
@@ -6,16 +6,18 @@ class AegisBroPlugins < Formula
   homepage 'http://www.sageaxcess.com'
   sha256 'bcf9c9c5d11c761f5696e752c85231f420a214b6352616a836781fad8fb35bc7'
 
+  depends_on "aegis-bro"
+
   def install
-    system "echo", "Installing bro plugins at #{Formula["bro"].lib}/bro/plugins"
+    system "echo", "Installing bro plugins at #{Formula["aegis-bro"].lib}/bro/plugins"
     system "echo", "WARNING: Your bro installation will be modified"
     system "echo", "         if you remove this package, you should reinstall bro."
-    system "cp -R -f 1.0.5/* #{prefix}"
-    system "mkdir -p #{Formula["bro"].lib}/bro/plugins"
-    system "ln -s #{prefix}/plugins/SageAxcess_ZMQ #{Formula["bro"].lib}/bro/plugins/SageAxcess_ZMQ"
-    system "ln -s #{prefix}/plugins/SageAxcess_SMB1 #{Formula["bro"].lib}/bro/plugins/SageAxcess_SMB1"
-    system "ln -s #{prefix}/plugins/SageAxcess_TDS #{Formula["bro"].lib}/bro/plugins/SageAxcess_TDS"
-    system "ln -s #{prefix}/plugins/SageAxcess_LDAP #{Formula["bro"].lib}/bro/plugins/SageAxcess_LDAP"
-    system "cp -R -f #{prefix}/scripts/* #{Formula["bro"].share}/bro"
+    system "cp -Rf 1.0.5/* #{prefix}"
+    system "mkdir -p #{Formula["aegis-bro"].lib}/bro/plugins"
+    system "ln -s #{prefix}/plugins/SageAxcess_ZMQ #{Formula["aegis-bro"].lib}/bro/plugins/SageAxcess_ZMQ"
+    system "ln -s #{prefix}/plugins/SageAxcess_SMB1 #{Formula["aegis-bro"].lib}/bro/plugins/SageAxcess_SMB1"
+    system "ln -s #{prefix}/plugins/SageAxcess_TDS #{Formula["aegis-bro"].lib}/bro/plugins/SageAxcess_TDS"
+    system "ln -s #{prefix}/plugins/SageAxcess_LDAP #{Formula["aegis-bro"].lib}/bro/plugins/SageAxcess_LDAP"
+    system "cp -Rf #{prefix}/scripts/* #{Formula["aegis-bro"].share}/bro"
   end
 end
